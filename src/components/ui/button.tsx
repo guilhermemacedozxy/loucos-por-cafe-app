74232c5a209f7a17de8042cc76b936bca55a6993
guilhermemacedozxy <contato.guilhermemacedo.dev@gmail.com>
2026-03-31 22:40:14 -0300

    fix: Ajustar o comportamento de hover do componente botão da interface.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground hover:border-[2px] hover:border-[--primary] hover:bg-[--card] hover:text-[--primary] hover:font-semibold",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
