f79595158cd666961ce917db8a08c983150878a1
guilhermemacedozxy <contato.guilhermemacedo.dev@gmail.com>
2026-04-03 18:33:26 -0300

    feat: Implementar validação de parâmetros da URL para garantir integridade e evitar falhas.

--- src/app/[welcome]/menu/page.tsx
+++ src/app/[welcome]/menu/page.tsx
@@ -1,10 +1,23 @@
+import { db } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+
 interface CoffeeShopMenuPageProps {
   params: Promise<{welcome: string}>;
+  searchParams: Promise<{consumptionMethod: string}>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
 }
 
 
-const CoffeeShopMenuPage = async ({params}: CoffeeShopMenuPageProps) => {
+const CoffeeShopMenuPage = async ({params, searchParams}: CoffeeShopMenuPageProps) => {
   const {welcome} = await params
+  const {consumptionMethod} = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const coffee = await db.coffeeShop.findMany({where: { slug: welcome }});
   return ( <h1>MENU DE CATEGORIAS PAGINA PRINCIPAL DA NOSSA CAFETERIA {welcome}</h1>);
 }
 
