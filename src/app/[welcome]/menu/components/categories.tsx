b541f070e04713b48695acd37bc16e7300dc98ff
guilhermemacedozxy <contato.guilhermemacedo.dev@gmail.com>
2026-04-05 20:33:31 -0300

    feat: Implementar renderização das categorias da cafeteria na interface.

--- src/app/[welcome]/menu/components/categories.tsx
+++ src/app/[welcome]/menu/components/categories.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { CoffeeShop, Prisma } from "@prisma/client";
 import { ClockIcon } from "lucide-react";
 import Image from "next/image";
@@ -15,8 +17,9 @@ interface CoffeeCategoriesProps {
 };
 
 const CoffeeCategories = ({ coffee }: CoffeeCategoriesProps) => (
-  <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl border-b border-opacity-5 border-black bg-[--secondary] p-5">
-    <div className="flex items-center gap-3">
+  <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl border-b border-opacity-5 border-black bg-[--secondary]">
+    <div className="p-5">
+      <div className="flex items-center gap-3">
       <div className="bg-[--primary] w-11 h-11 rounded-xl flex items-center">
         <Image src={coffee.avatarImageUrl} alt={coffee.name} width={65} height={65} />
       </div>
@@ -24,11 +27,26 @@ const CoffeeCategories = ({ coffee }: CoffeeCategoriesProps) => (
         <h2 className="font-semibold text-lg">{coffee.name}</h2>
         <p className="text-xs opacity-55">{coffee.description}</p>
       </div>
-    </div>
-    <div className="flex items-center gap-1 text-xs text-[--primary] mt-3">
+      </div>
+
+
+      <div className="flex items-center gap-1 text-xs text-[--primary] mt-3">
       <ClockIcon size={12} />
       <p>Aberto | 07:00 ás 22:00</p>
+      </div>
     </div>
+
+    <ScrollArea className="w-full">
+      <div className="flex w-max space-x-4 px-4 pt-0">
+        {coffee.menuCategories.map(category => 
+          <Button key={category.id} variant="secondary" size="sm" className="rounded-full border opacity-20 border-black p-4 hover:opacity-100 hover:bg-[--primary] hover:text-[--secondary] hover:border-none">
+            {category.name} 
+          </Button>
+        )}
+      </div>
+      <ScrollBar orientation="horizontal"/>
+    </ScrollArea>
+
   </div>
 )
 
