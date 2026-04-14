cb69497e6907f16818b7097e70449383f0b0c79c
guilhermemacedozxy <contato.guilhermemacedo.dev@gmail.com>
2026-04-13 22:49:20 -0300

    fix: Corrigir renderização de imagens externas no Next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    remotePatterns: [{hostname: "z58avo7iib.ufs.sh"}],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
