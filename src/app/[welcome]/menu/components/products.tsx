8b951b7a95457dc8947cf20ee7871de882926a35
guilhermemacedozxy <contato.guilhermemacedo.dev@gmail.com>
2026-04-16 23:25:58 -0300

    feat: Implementar navegação dinâmica para página de detalhe do produto via parâmetros.

--- src/app/[welcome]/menu/components/products.tsx
+++ src/app/[welcome]/menu/components/products.tsx
@@ -1,16 +1,18 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 interface ProductsProps {
   products: Product[]
 }
 
 const Products = ({ products }: ProductsProps) => {
+  const { welcome } = useParams<{ welcome: string }>();
   return (
     <div className="space-y-3 px-5">
       {products.map(product => (
-        <Link key={product.id} href="/" className="flex items-center justify-between gap-10 py-3 border-b border-opacity-5 border-black">
+        <Link key={product.id} href={`/${welcome}/menu/${product.id}`} className="flex items-center justify-between gap-10 py-3 border-b border-opacity-5 border-black">
 
           <div>
             <h3 className="text-sm font-normal">
