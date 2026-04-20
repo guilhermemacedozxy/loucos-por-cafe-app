9726a9b902c52d61815fac710c74ebe7f730227b
guilhermemacedozxy <contato.guilhermemacedo.dev@gmail.com>
2026-04-20 20:06:33 -0300

    feat: Exibir imagem dos produtos a partir da conexão com o banco de dados.

--- src/app/[welcome]/menu/[productId]/page.tsx
+++ src/app/[welcome]/menu/[productId]/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "@/lib/prisma";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
 interface ProductPageProps {
   params: Promise<{welcome: string, productId: string}>;
 }
@@ -5,8 +9,16 @@ interface ProductPageProps {
 
 const ProductPage = async ({params}: ProductPageProps) => {
   const { welcome, productId } = await params; 
+  const product = await db.product.findFirst({where: { id: productId }})
+  if (!product) {
+    return 
+      notFound();
+  }
   return (
     <>
+    <div className="relative w-full h-[300px]">
+      <Image src={product.imageUrl} alt={product.name} fill className="object-contain" />
+    </div>
       <h1>Product Page</h1>
       {welcome}
       {productId}
