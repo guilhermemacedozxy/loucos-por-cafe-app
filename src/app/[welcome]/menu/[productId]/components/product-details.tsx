a1f469b02b63064590ed244974217037f3271f3b
guilhermemacedozxy <contato.guilhermemacedo.dev@gmail.com>
2026-05-06 12:41:08 -0300

    feat: Exibir nome do produto na página de detalhes.

--- src/app/[welcome]/menu/[productId]/components/product-details.tsx
+++ src/app/[welcome]/menu/[productId]/components/product-details.tsx
@@ -10,22 +10,24 @@ interface ProductDetailsProps {
   } } }>;
 }
 
-const ProductDetails = ({ product }: ProductDetailsProps) => {
-  return (
-    <div className="relative z-50 rounded-t-3xl py-5 mt-[-1.5rem]">
-      <div>
-        <div className="flex items-center gap-1.5 px-5">
-          <div className="bg-[--primary] rounded-full">
-            <Image src={product.coffeeShop.avatarImageUrl} alt={product.coffeeShop.name} width={16} height={16}/>
-          </div>
-
-          <p className="text-xs opacity-50">
-            {product.coffeeShop.name}
-          </p>
+const ProductDetails = ({ product }: ProductDetailsProps) => (
+  <div className="relative z-50 rounded-t-3xl py-5 mt-[-1.5rem] p-5">
+    <div>
+      <div className="flex items-center gap-1.5">
+        <div className="bg-[--primary] rounded-full">
+          <Image src={product.coffeeShop.avatarImageUrl} alt={product.coffeeShop.name} width={16} height={16} />
         </div>
+
+        <p className="text-xs opacity-50">
+          {product.coffeeShop.name}
+        </p>
+
+
       </div>
+
+      <h2 className="text-xl font-semibold mt-1">{product.name}</h2>
     </div>
-  );
-}
+  </div>
+)
 
 export default ProductDetails;
