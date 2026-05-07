fdc7c20abe4099f908fc495478effb7e982414c9
guilhermemacedozxy <contato.guilhermemacedo.dev@gmail.com>
2026-05-07 16:06:24 -0300

    feat: Implementar funcionalidade de incrementar e decrementar quantidade.

--- src/app/[welcome]/menu/[productId]/components/product-details.tsx
+++ src/app/[welcome]/menu/[productId]/components/product-details.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/helpers/format-currency";
 import { Prisma } from "@prisma/client";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{ include: { coffeeShop: {
@@ -13,9 +16,17 @@ interface ProductDetailsProps {
   } } }>;
 }
 
-const ProductDetails = ({ product }: ProductDetailsProps) => (
-  <div className="relative z-50 rounded-t-3xl py-5 mt-[-1.5rem] p-5">
-    <div>
+const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const [quantity, setQuantity] = useState<number>(1);
+  const handleDecreaseQuantity = () => {
+    setQuantity(prev => prev - 1)
+  };
+  const handleIncreaseQuantity = () => {
+    setQuantity(prev => prev + 1)
+  };
+  return (
+    <div className="relative z-50 rounded-t-3xl py-5 mt-[-1.5rem] p-5">
+      <div>
       <div className="flex items-center gap-1.5">
         <div className="bg-[--primary] rounded-full">
           <Image src={product.coffeeShop.avatarImageUrl} alt={product.coffeeShop.name} width={16} height={16} />
@@ -33,19 +44,20 @@ const ProductDetails = ({ product }: ProductDetailsProps) => (
       <div className="flex items-center justify-between">
         <h3 className="text-xl font-semibold">{formatCurrency(product.price)}</h3>
       <div className="flex items-center gap-3 text-center">
-        <Button variant="destructive" className="h-8 w-8 rounded-xl">
+        <Button variant="destructive" className="h-8 w-8 rounded-xl" onClick={handleDecreaseQuantity}>
           <ChevronLeftIcon/>
         </Button>
 
-        <p className="w-4">1</p>
+        <p className="w-4">{quantity}</p>
 
-        <Button variant="outline" className="h-8 w-8 rounded-xl">
+        <Button variant="outline" className="h-8 w-8 rounded-xl" onClick={handleIncreaseQuantity}>
           <ChevronRightIcon/>
         </Button>
       </div>
       </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default ProductDetails;
